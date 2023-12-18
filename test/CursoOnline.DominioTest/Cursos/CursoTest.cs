5d3fa26e5e6ba0b4e49101e7746175a67c91f28d
Abner Costa <abnermoura@pmenos.com.br>
2023-12-18 18:26:34 -0300

    three tests added

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,76 @@
+﻿using ExpectedObjects;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            //Arrange
+            var cursoEsperado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            //Act
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo,
+                cursoEsperado.Valor);
+
+            //Assert
+            
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeVazioOuNulo(string nomeInvalido)
+        {
+            var cursoEsperado = new
+            {
+                Nome = nomeInvalido,
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() => 
+                new Curso(string.Empty, cursoEsperado.CargaHoraria,
+                cursoEsperado.PublicoAlvo,
+                cursoEsperado.Valor));
+        }
+    }
+
+
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException();
+            
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreeendedor
+    }
+}
